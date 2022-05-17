da01134c5a0eb3a3e0d5472b9dddbf2401705536
Sterling Devils <Michael.Wyrick@caci.com>
2022-05-17 17:04:48 -0400

    Example now runs and checks output

--- pkg/udp/udp_test.go
+++ pkg/udp/udp_test.go
@@ -26,15 +26,16 @@ func Example() {
 	}
 	defer udpcomp.Close()
 
-	// Example Write packet to UDP
-	in <- Packet{Addr: &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 9999}, Data: []byte("Hello from Us.")}
-
+	// Wait for 1 second, then send a packet to our self, and display it, exit after 3 seconds
 	for {
 		select {
-		case <-time.After(time.Second * 20):
-			return
+		case <-time.After(time.Second * 1):
+			in <- Packet{Addr: &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 9092}, Data: []byte("Hello from Us.")}
 		case p := <-udpcomp.OuputChan():
 			dispPacket(p) // Display the Packet
+			return
 		}
 	}
+
+	// Output: 127.0.0.1:9092: [72 101 108 108 111 32 102 114 111 109 32 85 115 46]
 }
