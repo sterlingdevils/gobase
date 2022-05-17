38bb7ad3a233c248a3592e8da73be2572be36dc9
Sterling Devils <Michael.Wyrick@caci.com>
2022-05-17 16:55:03 -0400

    more example updates

--- pkg/udp/udp_text.go
+++ pkg/udp/udp_text.go
@@ -12,23 +12,28 @@ func dispPacket(p Packet) {
 	fmt.Printf("%v: %v\n", p.Addr, p.Data)
 }
 
+// Example of how to create,receive and send packets
 func Example() {
+	wg := new(sync.WaitGroup)
+
 	in := make(chan Packet, 5)
 	defer close(in)
-	wg := new(sync.WaitGroup)
-	rx, err := New(wg, in, ":9092", SERVER, 1)
+
+	// Must pass in the input channel as we dont assume we own it
+	udpcomp, err := New(wg, in, ":9092", SERVER, 1)
 	if err != nil {
 		log.Fatalln("error creating UDP")
 	}
-	defer rx.Close()
+	defer udpcomp.Close()
 
+	// Example Write packet to UDP
 	in <- Packet{Addr: &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 9999}, Data: []byte("Hello from Us.")}
-	out := rx.OuputChan()
+
 	for {
 		select {
 		case <-time.After(time.Second * 20):
 			return
-		case p := <-out:
+		case p := <-udpcomp.OuputChan():
 			dispPacket(p) // Display the Packet
 		}
 	}
