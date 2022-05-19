2f900a28e9698104b0ef85caee958f35d91b9416
Sterling Devils <Michael.Wyrick@caci.com>
2022-05-19 10:53:33 -0400

    added new functions to serialnum, Breaking change

--- pkg/serialnum/serialnum.go
+++ pkg/serialnum/serialnum.go
@@ -1,23 +1,68 @@
 // Package serialnum adds and removes serial numbers to byte slices
 package serialnum
 
-import "crypto/rand"
+import (
+	"encoding/binary"
+	"errors"
+	"math/rand"
+	"sync/atomic"
+	"time"
+)
 
 const (
+	// number of bytes in a serial number
 	SERIALNUMSIZE = 8
 )
 
-// Adds serial number to byte slice
-func Add(in []byte) []byte {
+var (
+	// Holds the current incremented serial number
+	currentSn uint64
+)
+
+// init is called when this package is loaded
+func init() {
+	currentSn = 0
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Adds an incrementing serial number to byte slice
+func AddInc(in []byte) ([]byte, error) {
 	sn := make([]byte, SERIALNUMSIZE)
-	rand.Read(sn)
-	return append(sn, in...)
+	binary.LittleEndian.PutUint64(sn, currentSn)
+
+	// Add one to currentSn safely
+	atomic.AddUint64(&currentSn, 1)
+
+	return append(sn, in...), nil
+}
+
+// Adds serial number to byte slice
+func AddRandom(in []byte) ([]byte, error) {
+	sn := make([]byte, SERIALNUMSIZE)
+	_, err := rand.Read(sn)
+	if err != nil {
+		return nil, err
+	}
+
+	return append(sn, in...), nil
 }
 
 // Removes serial number from byte slice or returns empty array if the size is too small
-func Remove(in []byte) []byte {
+// returns the data that is after the sn, the sn, and an error it there was a problem
+func Remove(in []byte) (data []byte, sn []byte, err error) {
 	if len(in) < SERIALNUMSIZE {
-		return make([]byte, 0)
+		return nil, nil, errors.New("passed in slice is smaller than a serialnumber")
 	}
-	return in[SERIALNUMSIZE:]
+
+	return in[SERIALNUMSIZE:], in[:SERIALNUMSIZE], nil
+}
+
+// Sn will return a slice with the serial number
+// note: The returned slice has the same underlying array
+func Sn(in []byte) (sn []byte, err error) {
+	if len(in) < SERIALNUMSIZE {
+		return nil, errors.New("passed in slice is smaller than a serialnumber")
+	}
+
+	return in[:SERIALNUMSIZE], nil
 }
--- pkg/serialnum/serialnum_test.go
+++ pkg/serialnum/serialnum_test.go
@@ -11,9 +11,11 @@ func Example() {
 	in := []byte("Slice1")
 	fmt.Println(len(in))
 	fmt.Println(in)
-	in = serialnum.Add(in)
+
+	in, _ = serialnum.AddRandom(in)
 	fmt.Println(len(in))
-	in = serialnum.Remove(in)
+
+	in, _, _ = serialnum.Remove(in)
 	fmt.Println(in)
 	fmt.Println(len(in))
 	// Output:
@@ -22,4 +24,23 @@ func Example() {
 	// 14
 	// [83 108 105 99 101 49]
 	// 6
+}
+
+func ExampleAddInc() {
+	in := []byte("Slice1")
+	fmt.Println(len(in))
+	fmt.Println(in)
+
+	in, _ = serialnum.AddInc(in)
+	fmt.Println(in)
+
+	in2 := []byte("Slice2")
+	in2, _ = serialnum.AddInc(in2)
+	fmt.Println(in2)
+
+	// Output:
+	// 6
+	// [83 108 105 99 101 49]
+	// [0 0 0 0 0 0 0 0 83 108 105 99 101 49]
+	// [1 0 0 0 0 0 0 0 83 108 105 99 101 50]
 }
