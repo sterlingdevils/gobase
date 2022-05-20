8742885bb4c6f6ad1b98e457ed7f37a0f231e919
Sterling Devils <Michael.Wyrick@caci.com>
2022-05-20 15:19:56 -0400

    more clean up on container

--- pkg/chanbasedcontainer/chanbasedcontainer.go
+++ pkg/chanbasedcontainer/chanbasedcontainer.go
@@ -1,4 +1,17 @@
-// ordered container of T indexed by Ks
+/*
+  package chanbaedcontainer implements an ordered container with only channels as the API
+
+  The input channel is used to add things to the container.
+  The output channel will contain the head of the container when read
+  The delete channel is used to remove things from the container before the are read out of
+  the output channel.
+
+  This uses Go 1.18 generics,  Things must impement the Indexable interface:
+    has a method to return a comparable key
+
+  Things come in and go out the channels in order.  Things can be removed while in the container
+  by passing their key to the delete channel
+*/
 package chanbasedcontainer
 
 import (
@@ -19,8 +32,8 @@ type Indexable[K comparable] interface {
 }
 
 type ChanBasedContainer[K comparable, T Indexable[K]] struct {
-	objmap  map[K]T
-	objlist *list.List
+	tmap  map[K]T
+	tlist *list.List
 
 	inchan  chan T
 	outchan chan T
@@ -29,48 +42,73 @@ type ChanBasedContainer[K comparable, T Indexable[K]] struct {
 	ctx  context.Context
 	can  context.CancelFunc
 	once sync.Once
+
+	// Holds the thing we are trying to send
+	onetosend *T
 }
 
 //func (r *ChanBasedContainer[K, T])
 
 func (r *ChanBasedContainer[_, T]) addT(thing T) {
 	k := thing.Key()
-	if _, b := r.objmap[k]; b {
+	if _, b := r.tmap[k]; b {
 		return
 	}
 
-	r.objlist.PushBack(k)
-	r.objmap[k] = thing
+	r.tlist.PushBack(k)
+	r.tmap[k] = thing
 }
 
 func (r *ChanBasedContainer[K, _]) delK(index K) {
-	for curr := r.objlist.Front(); curr != nil; curr = curr.Next() {
+	// If we have one to delete, check if its the one we are waiting to send
+	if r.onetosend != nil {
+		if (*r.onetosend).Key() == index {
+			r.onetosend = nil
+			return
+		}
+	}
+
+	// Not onetosend so check in the continer
+	for curr := r.tlist.Front(); curr != nil; curr = curr.Next() {
 		val := curr.Value.(K)
 		if val == index {
-			r.objlist.Remove(curr)
-			delete(r.objmap, index)
+			r.tlist.Remove(curr)
+			delete(r.tmap, index)
 			break
 		}
 	}
 }
 
-func (r *ChanBasedContainer[K, T]) pop() T {
-	e := r.objlist.Front()
+// grab the head of the container or nil if we are empty
+func (r *ChanBasedContainer[K, T]) pop() *T {
+	if len(r.tmap) == 0 {
+		return nil
+	}
+
+	e := r.tlist.Front()
 	k := e.Value.(K)
-	t := r.objmap[k]
+	t := r.tmap[k]
 	r.delK(k)
-	return t
+	return &t
 }
 
-func (r *ChanBasedContainer[_, _]) IsEmpty() bool {
-	return len(r.objmap) == 0
-}
+// TODO:  Removed as these are not thread safe
+// func (r *ChanBasedContainer[_, _]) IsEmpty() bool {
+// 	if r.onetosend == nil {
+// 		return len(r.tmap) == 0
+// 	}
+// 	return false
+// }
 
-func (r *ChanBasedContainer[_, _]) QueueSize() int {
-	return len(r.objmap)
-}
+// func (r *ChanBasedContainer[_, _]) QueueSize() int {
+// 	if r.onetosend == nil {
+// 		return len(r.tmap)
+// 	}
+// 	return len(r.tmap) + 1
+// }
 
-// Add will place key onto the in channel
+// Add will place thing onto the in channel
+// this can block until ready
 func (r *ChanBasedContainer[_, T]) Add(thing T) {
 	r.inchan <- thing
 }
@@ -87,6 +125,7 @@ func (r *ChanBasedContainer[_, T]) Next() (T, error) {
 }
 
 // Delete will place key onto the delete channel
+// this can block until ready
 func (r *ChanBasedContainer[K, _]) Delete(key K) {
 	r.delchan <- key
 }
@@ -108,17 +147,25 @@ func (r *ChanBasedContainer[K, _]) DelChan() chan<- K {
 
 // Close the ChanBasedContainer
 func (r *ChanBasedContainer[_, _]) Close() {
+	// Cancel our context
 	r.can()
 	r.once.Do(func() {
 		close(r.outchan)
 	})
 }
 
+// mainloop
+// If the container is empty, only listen for
 func (r *ChanBasedContainer[_, T]) mainloop() {
 	defer chantools.RecoverFromClosedChan()
-	var onetosend *T
 	for {
-		if r.IsEmpty() {
+		// Check if we have one ready to send
+		if r.onetosend == nil {
+			r.onetosend = r.pop() // pop will return nil if one is not ready
+		}
+
+		if r.onetosend == nil {
+			// None to send so don't select on output channel
 			select {
 			case t := <-r.inchan:
 				r.addT(t)
@@ -128,24 +175,15 @@ func (r *ChanBasedContainer[_, T]) mainloop() {
 				return
 			}
 		} else {
-			if onetosend == nil {
-				o := r.pop()
-				onetosend = &o
-			}
+			// We have one to send so select on output channel
 			select {
+			case r.outchan <- *r.onetosend:
+				// Now that we sent it, clean onetosend so we get the next one
+				r.onetosend = nil
 			case t := <-r.inchan:
 				r.addT(t)
 			case k := <-r.delchan:
-				if onetosend == nil {
-					break
-				}
-				if (*onetosend).Key() == k {
-					onetosend = nil
-				} else {
-					r.delK(k)
-				}
-			case r.outchan <- *onetosend:
-				onetosend = nil
+				r.delK(k)
 			case <-r.ctx.Done():
 				return
 			}
@@ -153,14 +191,18 @@ func (r *ChanBasedContainer[_, T]) mainloop() {
 	}
 }
 
+// New returns a reference to a a container or error if there was a problem
+// for performance T should be a pointer
 func New[K comparable, T Indexable[K]]() (*ChanBasedContainer[K, T], error) {
-	objs := make(map[K]T)
-	objl := list.New()
-	ichan := make(chan T, CHANSIZE)
-	ochan := make(chan T, CHANSIZE)
-	dchan := make(chan K, CHANSIZE)
-	c, cancel := context.WithCancel(context.Background())
-	r := ChanBasedContainer[K, T]{objmap: objs, objlist: objl, inchan: ichan, outchan: ochan, delchan: dchan, ctx: c, can: cancel}
+	con, cancel := context.WithCancel(context.Background())
+	r := ChanBasedContainer[K, T]{
+		tmap:    make(map[K]T),
+		tlist:   list.New(),
+		inchan:  make(chan T, CHANSIZE),
+		outchan: make(chan T, CHANSIZE),
+		delchan: make(chan K, CHANSIZE),
+		ctx:     con,
+		can:     cancel}
 
 	go r.mainloop()
 
