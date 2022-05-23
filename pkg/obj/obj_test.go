c60f84f4a94214274bc5982d871e5895492f2b91
Sterling Devils <Michael.Wyrick@caci.com>
2022-05-23 15:52:34 -0400

    fixed Example after Ctx went to private

--- pkg/obj/obj_test.go
+++ pkg/obj/obj_test.go
@@ -10,10 +10,10 @@ import (
 // ExampleNew will show how to create one of us
 func ExampleNew() {
 	o, _ := obj.New(time.Millisecond * 100)
-	fmt.Println(o.Ctx.Err())
+	fmt.Println(o.Context().Err())
 
 	time.Sleep(time.Millisecond * 200)
-	fmt.Println(o.Ctx.Err())
+	fmt.Println(o.Context().Err())
 	// Output:
 	// <nil>
 	// context deadline exceeded
